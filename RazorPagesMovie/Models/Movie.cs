09c1041d06c0cc2898aa5cff53a3c2627e9f71df
BEOMUN PARK <sos891214@gmail.com>
2021-07-14 14:55:34 +0900

    init

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; } //ID 필드는 데이터베이스에서 기본 키 대신 필요
+        public String Title { get; set; }
+
+        [DataType(DataType.Date)] //특정 데이터 형식 지정         
+        public DateTime ReleaseDate { get; set; }
+        public String Genre { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
